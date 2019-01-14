a32a7996f5e22eecdf911319d669a17db8049fae
András Urbin <urbinandras@gmail.com>
2019-01-14 13:17:14 +0100

    save and load data done

---
+++ HistoricalDatas.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class HistoricalDatas{
+    
+    public void generateData(){
+        try{    
+        String[] placementStrings = {"1", "2", "3", "4"};
+        PrintWriter pw = new PrintWriter(new File("HistoricalData.csv"));
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<placementStrings.length; i++){
+            sb.append(placementStrings[i]);
+        }
+        pw.write(sb.toString());
+        pw.close();
+        }catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }
+    }
+
+    public void loadData(){
+        String csvFile = "HistoricalData.csv";
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] country = line.split(cvsSplitBy);
+
+                for(int i = 0; i<country.length; i++){
+                    System.out.println(country[i]);
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
-public class Main{
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class Main{
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number, where you'd like to go!");
+        int menuOptions = scanner.nextInt();
+        scanner.nextLine();
+        HistoricalDatas history = new HistoricalDatas();
+
+        switch(menuOptions){
+            case 1: System.out.println("First case: Show the best probability!");
+                    history.loadData();
+                    break;
+            case 2: System.out.println("Second case: Generate data");
+                    break;
+        }
     }
+
 }
