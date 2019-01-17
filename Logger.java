2458ad533c4848abe884bbd9d3afe9ca87437623
Gergő Papp <imgergopapp@gmail.com>
2019-01-17 13:22:30 +0100

    Gergo's version for final merge

--- Logger.java
+++ Logger.java
@@ -1,13 +1,8 @@
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 public class Logger{
-
-    //Constructor
-    // public Logger (String type, String errorMessage){
-    //     this.type = type;
-    //     this.errorMessage = errorMessage;
-    // }
 
     //Methods
     public void log(String type, String errorMessage){
@@ -22,5 +17,59 @@ public class Logger{
         double diffTimeInSeconds =  Math.abs(diffTime/1000);
         
         System.out.println("The task was finished in " + diffTimeInSeconds + " second(s).");
+    }
+
+    public int handleInputInt(String outputMessage){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(outputMessage);
+        int userInput = scanner.nextInt();
+        scanner.nextLine();
+        return userInput;
+    }
+
+    public String handleInputString(String outputMessage){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(outputMessage);
+        String userInput = scanner.next();
+        scanner.nextLine();
+        return userInput;
+    }
+
+    public void menu(){
+        HistoricalDatas history = new HistoricalDatas();
+        Logger log = new Logger();
+        System.out.println("    0. Quit");
+        System.out.println("    1. Generate n rounds of the race!");
+        System.out.println("    2. Show one round of the race!");
+        System.out.println("    3. Show statistics!");
+
+        boolean notQuit = true;
+        while (notQuit){
+            int menuOptions = handleInputInt("Enter a number, where you'd like to go!");
+            switch(menuOptions){
+                case 0: notQuit = false;
+                        break;
+                case 1: int rounds = handleInputInt("How many runs would you like to create?");
+                        Date currentDate1 = new Date(); 
+                        for(int i = 0; i<rounds; i++){
+                            HistoricalDatas runningHistory = new HistoricalDatas();
+                            runningHistory.generateData();
+                        }                    
+                        Date currentDate2 = new Date();
+                        log.timeDiffInSeconds(currentDate1, currentDate2);
+                        break;
+                case 2: System.out.println("Showing one test run!\n");
+                        history.printOneRound();
+                        String oneRoundWinner = history.getFirstPlace();
+                        System.out.println("\nThe winner of this round is: " + oneRoundWinner);
+                        break;
+                case 3: System.err.println("Showing statistics!");
+                        //Printing useful stats!
+                        break;
+            }
+        }
+        
     }
 }
--- Main.java
+++ Main.java
@@ -1,41 +1,17 @@
-import java.util.Scanner;
-import java.util.Date;
+// import java.util.Scanner;
+// import java.util.Date;
 
 public class Main{
-    
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number, where you'd like to go!");
-        System.out.println("    1. Generate n rounds of the race!");
-        System.out.println("    2. Show one round of the race!");
-        System.out.println("    3. Show statistics!");
-        int menuOptions = scanner.nextInt();
-        scanner.nextLine();
-        int rounds;
-        HistoricalDatas history = new HistoricalDatas();
         Logger log = new Logger();
+        log.menu();
+        
+        
+        
 
         
-        switch(menuOptions){
-            case 1: System.out.println("How many runs would you like to create?");
-                    rounds = scanner.nextInt();
-                    scanner.nextLine();
-                    Date currentDate = new Date(); 
-                    for(int i = 0; i<rounds; i++){
-                        HistoricalDatas runningHistory = new HistoricalDatas();
-                        runningHistory.generateData();
-                    }
-                    Date currentDate2 = new Date();
-                    log.timeDiffInSeconds(currentDate, currentDate2);
-                    break;
-            case 2: System.out.println("Showing one test run!\n");
-                    history.printOneRound();
-                    String oneRoundWinner = history.getFirstPlace();
-                    System.out.println("\nThe winner of this round is: " + oneRoundWinner);
-                    break;
-            case 3: System.err.println("Showing statistics!");
-                    //Printing useful stats!
-        }
+        
+                    
     }
     
 }
